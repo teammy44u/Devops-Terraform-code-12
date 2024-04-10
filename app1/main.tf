8ee090429121f38352190787c48677894fe83123
jayjay~ <oluwatimilehinajayi@yahoo.com>
2024-04-09 22:12:17 -0400

    working

---
+++ app1/main.tf
@@ -0,0 +1,7 @@
+module "key" {
+    source = "../keypair"
+    region_name = "us-east-1"
+    key_name = "jayjay89"
+  
+}
+
---
+++ dev-app/main.tf
@@ -0,0 +1,8 @@
+module "rds1" {
+    source = "../module/rds"
+  
+}
+
+module "key" {
+  
+}
